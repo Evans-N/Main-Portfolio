06805854f2635c1995f736712d7379f54885af2c
Nathan Evans <nevans.dev@gmail.com>
2019-11-25 19:41:56 -0500

    Added new cards for projects and started integrating new CSS.

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log('Hi mom!')
-    let mainNav = document.getElementById('js-menu');
-    let navBarToggle = document.getElementById('js-navbar-toggle');
-
-    navBarToggle.addEventListener('click', function () {
-        console.log('Hi Dad!')
-        mainNav.classList.toggle('active');
-    });
-    window.onscroll = function() {scrollFunction()};
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            document.getElementById("header").style.fontSize = "30px";
-        } else {
-            document.getElementById("header").style.fontSize = "90px";
-        }
-    }
-});
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = window.M.Sidenav.init(elems);
+  });
